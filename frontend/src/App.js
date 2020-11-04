3dc2366f114de3cc5f908d97efe369264967c614
YuriyTikhonoff <yuryitikhonoff@gmail.com>
2020-11-05 00:00:23 +0200

    Provide fetching items from the Express server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Homescreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3"> 
         <Container>
-          <Homescreen />
+          <Route path='/' component={Homescreen} exact />
+          <Route path='/products/:id' component={ProductScreen} exact />
+
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
