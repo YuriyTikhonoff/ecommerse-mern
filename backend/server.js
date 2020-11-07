e92771be910ee92e84f94f4506e9c05e6877a60f
YuriyTikhonoff <yuryitikhonoff@gmail.com>
2020-11-08 00:49:15 +0200

    add error handlers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMidleware.js'
 
 dotenv.config()
 
@@ -11,18 +12,35 @@ connectDB()
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log('This is a result of working middleware')
+    next()
+})
+
+
+
+
+app.use('/api/products', productRoutes)
+
+
+
 app.get('/', (req, res) => { 
     res.send('Api is running')
 })
 
-app.get('/api/products', (req, res) => { 
-    res.json(products)
-})
+// app.get('/api/products', (req, res) => { 
+//     res.json(products)
+// })
 
-app.get('/api/products/:id', (req, res) => { 
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+// app.get('/api/products/:id', (req, res) => { 
+//     const product = products.find(p => p._id === req.params.id)
+//     res.json(product)
+// })
+
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 
 const PORT = process.env.PORT || 5000
