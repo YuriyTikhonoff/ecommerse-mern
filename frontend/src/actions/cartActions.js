efe3fccb730da1cb65e8ab67e6884072851ee026
YuriyTikhonoff <yuryitikhonoff@gmail.com>
2020-11-12 00:12:47 +0200

    Develop cartScreen component

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -17,6 +17,6 @@ export const addToCart = (id, qty) => async(dispatch, getState) => {
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-    localStorage.setItem('test','successful')
+   
 
 }
