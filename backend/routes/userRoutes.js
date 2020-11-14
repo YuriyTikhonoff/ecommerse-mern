d455383c28b06c773d2c9ab3f26f368a16bdf479
YuriyTikhonoff <yuryitikhonoff@gmail.com>
2020-11-14 23:42:15 +0200

    Add create user functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,13 @@
 import express from 'express'
 const router = express.Router()
 
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 
-//@desc     Fetch all products
-//@route    Get /api/products
-//@access   Public
-
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 
 export default router
